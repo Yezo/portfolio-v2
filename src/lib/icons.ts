5e0901525c9942240b7d38bff26954fd4a738f63
Kev <sulpeum@gmail.com>
2023-05-01 18:16:06 -0400

    add icon data

---
+++ src/lib/icons.ts
@@ -0,0 +1,140 @@
+export const SKILLS = [
+  {
+    name: "React",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    category: "fundamental",
+  },
+  {
+    name: "TailwindCSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    category: "fundamental",
+  },
+  {
+    name: "Redux Toolkit",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "Javascript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    category: "fundamental",
+  },
+  {
+    name: "TypeScript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    category: "fundamental",
+  },
+  {
+    name: "Next.js",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "HTML",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    category: "fundamental",
+  },
+  {
+    name: "CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    category: "fundamental",
+  },
+
+  {
+    name: "Node.js",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "Express",
+    icon: "/express.png",
+    category: "familiarity",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+    category: "familiarity",
+  },
+
+  {
+    name: "MySQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    category: "beginner",
+  },
+
+  {
+    name: "Figma",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    category: "beginner",
+  },
+  {
+    name: "SASS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "Bootstrap",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "MaterialUI",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    category: "familiarity",
+  },
+  {
+    name: "Jest",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+    category: "beginner",
+  },
+  {
+    name: "Git",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    category: "fundamental",
+  },
+  {
+    name: "Jotai",
+    icon: "/jotai.png",
+    category: "fundamental",
+  },
+  {
+    name: "Chart.js",
+    icon: "/chartjs.png",
+    category: "beginner",
+  },
+  {
+    name: "Framer Motion",
+    icon: "/framer.webp",
+    category: "familiarity",
+  },
+  {
+    name: "HeadlessUI",
+    icon: "/headlessUI.png",
+    category: "familiarity",
+  },
+  {
+    name: "RadixUI",
+    icon: "/radixUI.png",
+    category: "familiarity",
+  },
+  {
+    name: "Postman",
+    icon: "/postman.webp",
+    category: "familiarity",
+  },
+  {
+    name: "Leaflet",
+    icon: "/reactleaflet.png",
+    category: "familiarity",
+  },
+  {
+    name: "React Router",
+    icon: "/reactrouter.png",
+    category: "familiarity",
+  },
+  {
+    name: "Vite",
+    icon: "/vite.png",
+    category: "familiarity",
+  },
+]
