19b77bc966763d3a541138e368ee3b19728c1e95
Kev <sulpeum@gmail.com>
2023-05-03 18:57:50 -0400

    change font size

--- src/components/Typography/Paragraph.tsx
+++ src/components/Typography/Paragraph.tsx
@@ -1,6 +1,6 @@
 export default function Paragraph({ children }: { children: React.ReactNode }) {
   return (
-    <p className="font-mplus text-[0.85rem] leading-7 text-[#444444] [&:not(:first-child)]:mt-6">
+    <p className="font-mplus text-[0.9rem] leading-7 text-[#444444] [&:not(:first-child)]:mt-6">
       {children}
     </p>
   )
