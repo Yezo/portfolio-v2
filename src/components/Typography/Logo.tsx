60af5904121d9f5b24a78dd6b0fcc55344a1c265
Kev <sulpeum@gmail.com>
2023-05-07 19:41:01 -0400

    change logo to a link

--- src/components/Typography/Logo.tsx
+++ src/components/Typography/Logo.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link"
+
 export default function Logo() {
-  return <div className="select-none font-faustina text-4xl tracking-tighter">kv.</div>
+  return (
+    <Link className="select-none font-faustina text-4xl tracking-tighter" href="/">
+      {"kv."}
+    </Link>
+  )
 }
