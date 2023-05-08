ba5bd32fbcc1233eaf2a2fb2d5f54c0fcb0273a9
Kev <sulpeum@gmail.com>
2023-05-08 00:08:10 -0400

    css adjustments

--- src/components/Typography/Subtitle.tsx
+++ src/components/Typography/Subtitle.tsx
@@ -1,3 +1,3 @@
 export default function Subtitle({ children }: { children: React.ReactNode }) {
-  return <p className="text-xl text-muted-foreground">{children}</p>
+  return <p className="font-rubik text-xs font-light tracking-wide text-[#aaaaaa]">{children}</p>
 }
--- src/components/Typography/TitleH1.tsx
+++ src/components/Typography/TitleH1.tsx
@@ -1,5 +1,3 @@
 export default function TitleH1({ children }: { children: React.ReactNode }) {
-  return (
-    <h1 className="pb-2 font-alegreya text-2xl font-bold text-[#2b2b2b] lg:text-4xl">{children}</h1>
-  )
+  return <h1 className="pb-2 font-alegreya text-2xl font-bold lg:text-4xl">{children}</h1>
 }
