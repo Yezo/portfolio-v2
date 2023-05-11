94ab2adb5a540dde362593e78d58a156f78e85be
Kev <sulpeum@gmail.com>
2023-05-11 16:44:03 -0400

    add text content for trackr

--- src/app/works/trackr/page.tsx
+++ src/app/works/trackr/page.tsx
@@ -36,23 +36,18 @@ export default function TrackrPage() {
 
       <section className="mt-8 space-y-6">
         <div>
-          <TitleH2>The Problem</TitleH2>
-          <Paragraph>{`???`}</Paragraph>
-        </div>
-
-        <div>
-          <TitleH2>The Solution</TitleH2>
-          <Paragraph>{`???`}</Paragraph>
+          <TitleH2>The Objective</TitleH2>
+          <Paragraph>{`Trackr is a way to keep track of all your job applications in one place. It allows for adding different jobs, editing existing applications, and also the removal of old applications. It also keeps track of certain statistics like how many are currently pending or rejected. `}</Paragraph>
         </div>
 
         <div>
           <TitleH2>Challenges</TitleH2>
-          <Paragraph>{`???`}</Paragraph>
+          <Paragraph>{`I wanted to create a small CRUD project without the use of a database to store data. Since my other projects have never dealt with LocalStorage, I thought this would be a good project to learn more about it. All the job applications are stored in LocalStorage which meant if the user came back on a different day to add more job applications to Trackr, the previous applications would still be there.`}</Paragraph>
         </div>
 
         <div>
           <TitleH2>Areas of improvement</TitleH2>
-          <Paragraph>{`???`}</Paragraph>
+          <Paragraph>{`The single most outstanding imporvement would obviously be to expand this project to use a CRUD API and implementing user authentication for the adding, editing, removal, and storing of all job applications. It would be a more streamlined experience and it would also prevent users from losing their existing data in cases where the user uses a different device for their job applications. `}</Paragraph>
         </div>
       </section>
 
