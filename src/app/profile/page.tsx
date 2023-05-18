37bc998f9b4e8dbb8cdf7ea82676b83a653c456d
Kev <sulpeum@gmail.com>
2023-05-18 13:30:04 -0400

    edit profile content

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -22,9 +22,9 @@ export default function ProfilePage() {
         <TitleH1>{`Hey, call me Kev 👋`}</TitleH1>
       </div>
 
-      <Paragraph>{`As a design-focused Software Engineer, I am most experienced with the Javascript ecosystem (React, TS, Next.js, Redux, Node). I currently specialize in crafting responsive web applications with a mobile-first approach. My passion thrives in front-end web positions where creative design meets web development.  `}</Paragraph>
+      <Paragraph>{`As a design-focused Software Engineer, I am most experienced with the Javascript ecosystem (React, TS, Next.js, Redux, Node). My current specialization revolves around crafting responsive web applications with a mobile-first approach. I am truly passionate about front-end web positions where the worlds of creative design and web development intersect.`}</Paragraph>
 
-      <Paragraph>{`Video games have been a large part of my life and I contribute that to a game developer's ability to fully immerse a player into the experience. As much as players love the atmosphere, the environment, and the interactions within a game, I hope to replicate those moments for others through an enriching and powerful web experience.`}</Paragraph>
+      <Paragraph>{`Video games have played a significant role in shaping my life, as they have the incredible ability to fully immerse players in unforgettable experiences. I attribute my passion for creating immersive experiences to game developers who skillfully craft atmospheres, environments, and interactions within games. It is my goal to replicate those captivating moments and bring them to others through enriching and powerful web experiences. `}</Paragraph>
 
       <section className="mt-6">
         <TitleH2>Contact</TitleH2>
