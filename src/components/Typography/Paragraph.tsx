7ec780fbfb51e33b3b0c3e589ce6a48fbc17f7e4
Kev <sulpeum@gmail.com>
2023-06-21 15:34:38 -0400

    change font to rubik

--- src/components/Typography/Paragraph.tsx
+++ src/components/Typography/Paragraph.tsx
@@ -1,6 +1,6 @@
 export default function Paragraph({ children }: { children: React.ReactNode }) {
   return (
-    <p className="font-inter text-[0.95rem] leading-[1.6] tracking-tight [word-spacing:-.02em] [&:not(:first-child)]:mt-4">
+    <p className="font-rubik text-[0.95rem] leading-[1.6] tracking-tight [word-spacing:-.02em] [&:not(:first-child)]:mt-4">
       {children}
     </p>
   )
