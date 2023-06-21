989560e093cafd45247d5821069b3b2dff293163
Kev <sulpeum@gmail.com>
2023-06-21 14:12:50 -0400

    change logo text from gray to off-white

--- src/components/Typography/Logo.tsx
+++ src/components/Typography/Logo.tsx
@@ -2,7 +2,10 @@ import Link from "next/link"
 
 export default function Logo() {
   return (
-    <Link className="select-none font-faustina text-4xl tracking-tighter" href="/">
+    <Link
+      className="select-none font-faustina text-4xl tracking-tighter dark:text-[#F3F4F6]"
+      href="/"
+    >
       {"kv."}
     </Link>
   )
