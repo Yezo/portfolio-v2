35494f4108433fd1bbdcdafa6f5d8afce06730a0
Kev <sulpeum@gmail.com>
2023-06-21 14:42:50 -0400

    remove underline + edit text

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,56 +20,55 @@ export const metadata = {
 export default function ProjectsPage() {
   return (
     <MainContainer>
-      <div className="border-b border-[#2b2b2b]/20 pb-1 dark:border-[#fff]/20">
-        <TitleH1>{`Projects`}</TitleH1>
-      </div>
+      <TitleH1>{`Projects`}</TitleH1>
+
       <section className="mt-6">
         <ProjectBox
           imageURL={arkoko}
           imageALT="Picture of Arkoko's brand logo"
-          title="Arkoko"
+          title="arkoko."
           description="Helping gamers minimize daily micro-decisions through data-driven analysis"
           caseLink="/projects/arkoko"
         />
         <ProjectBox
           imageURL={aniflux}
           imageALT="Picture of AniFlux's brand logo"
-          title="AniFlux"
+          title="aniflux."
           description="Exploring a modern re-design to the anime and manga database experience"
           caseLink="/projects/aniflux"
         />
         <ProjectBox
           imageURL={riskthinking}
           imageALT="Picture of riskthinking's brand logo"
-          title="RiskThinking"
-          description="Work sample for riskthinking.ai visualizing climate risk datasets"
+          title="riskthinking."
+          description="Web application as a work sample for riskthinking.ai visualizing climate risk datasets"
           caseLink="/projects/riskthinking"
         />
         <ProjectBox
           imageURL={kv}
           imageALT="Picture of Kevin Vo's brand logo"
-          title="My Portfolio"
-          description="A deep dive on a junior developer perspective to self-branding"
+          title="my portfolio."
+          description="A deep dive on a junior web developer's perspective to self-branding"
           caseLink="/projects/portfolio"
         />
         <ProjectBox
           imageURL={snippits}
           imageALT="Picture of Snippit's brand logo"
-          title="Snippits"
+          title="snippits."
           description="Code snippet repository built for programmers with the memory of a goldfish "
           caseLink="/projects/snippits"
         />
         <ProjectBox
           imageURL={kore}
           imageALT="Picture of Kore's brand logo"
-          title="Kore"
+          title="kore."
           description="Minigame where players can learn the Korean alphabet"
           caseLink="/projects/kore"
         />
         <ProjectBox
           imageURL={trackr}
           imageALT="Picture of Trackr's brand logo"
-          title="Trackr"
+          title="trackr."
           description="Keep track of your job applications by adding, editing, or removing job applications as needed"
           caseLink="/projects/trackr"
         />
