ce60298ff104958b4eda1f370c6a218889182795
Kev <sulpeum@gmail.com>
2023-06-21 14:42:27 -0400

    change font + add underline

--- src/components/Typography/TitleH1.tsx
+++ src/components/Typography/TitleH1.tsx
@@ -1,6 +1,6 @@
 export default function TitleH1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="pb-2 font-alegreya text-2xl font-bold [word-spacing:-.04em] dark:text-[#F3F4F6] lg:text-4xl">
+    <h1 className="border-b border-[#2b2b2b]/20 pb-2 font-rubik text-2xl font-medium tracking-tighter [word-spacing:-.04em] dark:border-[#fff]/20 dark:text-[#F3F4F6] lg:text-2xl">
       {children}
     </h1>
   )
