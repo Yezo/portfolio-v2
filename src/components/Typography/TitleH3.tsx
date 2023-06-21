ed39173e0181262ebea9e4468d870d48cc636949
Kev <sulpeum@gmail.com>
2023-06-21 14:42:37 -0400

    change font + add underline

--- src/components/Typography/TitleH3.tsx
+++ src/components/Typography/TitleH3.tsx
@@ -1,6 +1,6 @@
 export default function TitleH3({ children }: { children: React.ReactNode }) {
   return (
-    <h3 className="scroll-m-20 font-alegreya text-xl font-semibold tracking-wide dark:text-white">
+    <h3 className=" pb-2 font-rubik text-2xl font-medium tracking-tighter transition-colors [word-spacing:-.04em] first:mt-0 dark:text-[#F3F4F6] lg:text-xl">
       {children}
     </h3>
   )
