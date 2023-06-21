de6cef70851308049937fbbe4e6afd2eaa65c5f1
Kev <sulpeum@gmail.com>
2023-06-21 14:43:05 -0400

    remove underline + edit text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,9 @@ const ProjectBox = dynamic(() => import("@/components/Projects/ProjectBox"))
 export default function Home() {
   return (
     <MainContainer>
-      <div className="border-b border-[#2b2b2b]/20 pb-1 dark:border-[#fff]/20">
-        <TitleH1>{`Kevin Vo `}</TitleH1>
-      </div>
-      <section className="mb-8 mt-4">
+      <TitleH1>{`Kevin Vo`}</TitleH1>
+
+      <section className="mb-12 mt-4">
         <Paragraph>{`I'm a design-focused developer specializing in crafting user-centered digital experiences through creativity and strategic design. By understanding user needs, I create visually compelling interfaces that enhance usability and drive positive interactions. `}</Paragraph>
         <Paragraph>{`I am currently actively seeking new opportunities. If you are in search of a developer who possesses a strong eye for design, I am extremely excited to collaborate with a team of creative designers and developers.`}</Paragraph>
         <Paragraph>
@@ -35,25 +34,26 @@ export default function Home() {
       </section>
 
       <TitleH2>Selected projects</TitleH2>
+
       <div className="mt-6">
         <ProjectBox
           imageURL={arkoko}
           imageALT="Picture of Arkoko's brand logo"
-          title="Arkoko"
+          title="arkoko."
           description="Helping gamers minimize daily micro-decisions through data-driven analysis"
           caseLink="/projects/arkoko"
         />
         <ProjectBox
           imageURL={aniflux}
           imageALT="Picture of AniFlux's brand logo"
-          title="AniFlux"
+          title="aniflux."
           description="Exploring a modern re-design to the anime and manga database experience"
           caseLink="/projects/aniflux"
         />
         <ProjectBox
           imageURL={riskthinking}
           imageALT="Picture of riskthinking's brand logo"
-          title="RiskThinking"
+          title="riskthinking."
           description="Work sample for riskthinking.ai visualizing climate risk datasets"
           caseLink="/projects/riskthinking"
         />
