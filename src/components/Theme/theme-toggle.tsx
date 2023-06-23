3618ff6006c5e14ebc4e47557b1ba429a323814d
Kev <sulpeum@gmail.com>
2023-06-23 15:18:36 -0400

    add sun and moon icon theme toggle

--- src/components/Theme/theme-toggle.tsx
+++ src/components/Theme/theme-toggle.tsx
@@ -1,17 +1,37 @@
 "use client"
 
 import * as React from "react"
+import { useState, useEffect } from "react"
 import { useTheme } from "next-themes"
+import { SunIcon, MoonIcon } from "lucide-react"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const [clicked, setClicked] = useState(false)
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  //Enable if you want no animation on the sun and moon icons on page load
+  // const handleClick = () => {
+  //   setTheme(theme === "light" ? "dark" : "light")
+  //   setClicked(true)
+  // }
 
   return (
     <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      {/* <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" /> */}
-      <div>Light</div>
-      <div>Dark</div>
+      {theme === "light" ? (
+        <SunIcon size={18} className="animate-in fade-in duration-700" />
+      ) : (
+        <MoonIcon size={18} className="animate-in fade-in duration-700" />
+      )}
       <span className="sr-only">Toggle theme</span>
     </button>
   )
