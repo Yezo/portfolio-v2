f252f8dbfedc1c27a1513f5f1e6bc76516b75ad3
Kev <sulpeum@gmail.com>
2023-06-27 16:38:58 -0400

    add hover states

--- src/components/Typography/WordLink.tsx
+++ src/components/Typography/WordLink.tsx
@@ -10,7 +10,7 @@ export default function WordLink({ children, url }: Props) {
     <Link
       href={url}
       target="_blank"
-      className="font-rubik text-[0.95rem] font-medium leading-[1.6] tracking-tight [word-spacing:-.02em] hover:underline dark:text-[#F3F4F6] [&:not(:first-child)]:mt-4 "
+      className="font-rubik text-[0.95rem] font-medium leading-[1.6] tracking-tight underline transition-colors duration-300 ease-in-out [word-spacing:-.02em] dark:text-[#F3F4F6] hover:dark:text-[#adadad] [&:not(:first-child)]:mt-4"
     >
       {children}
     </Link>
