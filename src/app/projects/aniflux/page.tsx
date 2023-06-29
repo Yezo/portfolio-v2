8503ef73f3dababb7e3bc14d5636ec2bcbdc2ccd
Kev <sulpeum@gmail.com>
2023-06-29 14:13:08 -0400

    update text

--- src/app/projects/aniflux/page.tsx
+++ src/app/projects/aniflux/page.tsx
@@ -62,10 +62,10 @@ export default function AniFluxPage() {
 
           <ProjectFigureImage src={AniFluxInfographThree} alt="Image of aniflux's website" />
           <Paragraph>
-            {`Regrettably, the JikanAPI, while offering a vast amount of data, has certain limitations. Notably, it lacks complete data for more obscure anime titles, which can result in incomplete information being displayed. Additionally, as a free API, it imposes rate limits to prevent an excessive number of requests. This means that users visiting AniFlux may encounter missing data due to rate limiting. `}
+            {`The JikanAPI, while offering a vast amount of data, has certain limitations. Notably, it lacks complete data for more obscure anime titles, which can result in incomplete information being displayed. Additionally, as a free API, it imposes rate limits to prevent an excessive number of requests. This means that users visiting AniFlux may encounter missing data due to rate limiting. `}
           </Paragraph>
           <Paragraph>
-            {`While the JikanAPI proves to be a valuable resource, its usage presented challenges during development, as I frequently encountered "429 Too Many Requests" errors when testing components. Consequently, populating pages with multiple pieces of content from the API became difficult, as it was necessary to adhere to the strict limit of no more than 3 requests per second which meant I could not show extra data from more than 2-3 sources for a single page.`}
+            {`JikanAPI also presented challenges during development, as I frequently encountered "429 Too Many Requests" errors when testing components. Consequently, populating pages with multiple pieces of content from the API became difficult, as it was necessary to adhere to the strict limit of no more than 3 requests per second which meant I could not show extra data from more than 2-3 sources for a single page.`}
           </Paragraph>
         </div>
       </section>
