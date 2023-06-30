ea3c102240b4042e44f6b4a3b091c805018c9a8d
Kev <sulpeum@gmail.com>
2023-06-30 13:15:37 -0400

    update image links

--- src/app/projects/aniflux/page.tsx
+++ src/app/projects/aniflux/page.tsx
@@ -1,7 +1,7 @@
 import aniflux from "../../../../public/aniflux.png"
-import AniFluxInfographOne from "../../../../public/aniflux/AniFluxInfographOne.png"
-import AniFluxInfographTwo from "../../../../public/aniflux/AniFluxInfographTwo.png"
-import AniFluxInfographThree from "../../../../public/aniflux/AniFluxInfographThree.png"
+import AniFluxInfograph1 from "../../../../public/aniflux/AniFluxInfograph1.png"
+import AniFluxInfograph2 from "../../../../public/aniflux/AniFluxInfograph2.png"
+import AniFluxInfograph3 from "../../../../public/aniflux/AniFluxInfograph3.png"
 import dynamic from "next/dynamic"
 
 const MainContainer = dynamic(() => import("@/components/Main/MainContainer"))
@@ -49,18 +49,18 @@ export default function AniFluxPage() {
 
       <section className="mt-8 space-y-16">
         <div>
-          <ProjectFigureImage src={AniFluxInfographOne} alt="Image of aniflux's website" />
+          <ProjectFigureImage src={AniFluxInfograph1} alt="Image of aniflux's website" />
           <Paragraph>
             {`Many anime and manga-related websites fall short in delivering a modern design and user-friendly experience. These platforms often suffer from information overload, bombarding users with an overwhelming amount of unnecessary data that hinders their ability to focus on the content they seek. The excessive clutter and irrelevant details make it arduous for users to digest and absorb the essential information they are looking for, be it news updates, reviews, or insightful articles.`}
           </Paragraph>
           <Paragraph>{`Consequently, navigating through such websites can prove to be a challenging task. A lack of a clear user interface make it frustrating for users to navigate around the website and find what they need. Simplifying the interface, implementing logical menu structures, and enhancing search functionalities are crucial steps toward creating a more friendly user experience.`}</Paragraph>
 
-          <ProjectFigureImage src={AniFluxInfographTwo} alt="Image of aniflux's website" />
+          <ProjectFigureImage src={AniFluxInfograph2} alt="Image of aniflux's website" />
           <Paragraph>
             {`AniFlux is my personal take on an anime and manga website, crafted using modern technologies to deliver a visually appealing and modern experience. With a focus on streamlining the content, unnecessary bloat is eliminated, allowing the detailed information of each anime or manga to shine. The intuitive navigation system empowers users to effortlessly navigate between their desired anime or manga. `}
           </Paragraph>
 
-          <ProjectFigureImage src={AniFluxInfographThree} alt="Image of aniflux's website" />
+          <ProjectFigureImage src={AniFluxInfograph3} alt="Image of aniflux's website" />
           <Paragraph>
             {`The JikanAPI, while offering a vast amount of data, has certain limitations. Notably, it lacks complete data for more obscure anime titles, which can result in incomplete information being displayed. Additionally, as a free API, it imposes rate limits to prevent an excessive number of requests. This means that users visiting AniFlux may encounter missing data due to rate limiting. `}
           </Paragraph>
