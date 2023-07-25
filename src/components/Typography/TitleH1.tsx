84de924e261926c17f2435a2eb1c6712761f5915
Kev <sulpeum@gmail.com>
2023-07-25 15:54:23 -0400

    change font and weights

--- src/components/Typography/TitleH1.tsx
+++ src/components/Typography/TitleH1.tsx
@@ -1,6 +1,6 @@
 export default function TitleH1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="font-rubik text-2xl font-medium tracking-tighter [word-spacing:-.04em]  dark:text-[#F3F4F6] lg:text-2xl">
+    <h1 className="font-spectral text-2xl font-medium tracking-tighter transition-colors  [word-spacing:-.04em]  dark:text-[#F3F4F6] lg:text-4xl">
       {children}
     </h1>
   )
