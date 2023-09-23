07e7aaab21334a344102fc69237564246c1a9d5b
Kev <sulpeum@gmail.com>
2023-09-23 18:50:52 -0400

    styles: fix mobile nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -26,14 +26,14 @@ export default function Navbar() {
 
   return (
     <>
-      <aside className="pb-10 font-faustina text-zinc-800 dark:text-foreground md:w-[150px] md:flex-shrink-0">
+      <aside className="px-4 pb-10 font-faustina text-zinc-800 dark:text-foreground md:w-[150px] md:flex-shrink-0">
         <div className="space-y-4 lg:sticky lg:top-20 lg:space-y-10">
           <div className="flex flex-col md:flex-row ">
             <Logo />
           </div>
 
           <nav
-            className="relative flex scroll-pr-6 flex-row items-start tracking-wider md:relative md:flex-col md:overflow-auto"
+            className="relative flex scroll-pr-6 flex-row items-center gap-4 tracking-wider md:relative md:flex-col md:items-start md:gap-0 md:overflow-auto"
             id="nav"
           >
             <ul className="mb-2 mt-2 flex flex-row gap-4 space-x-0 md:mt-0 md:flex-col">
