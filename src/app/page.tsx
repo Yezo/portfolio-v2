8af5e62f765978368988b41278a9116c90b6e737
Kev <sulpeum@gmail.com>
2024-03-06 14:11:57 -0500

    feat: update intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Paragraph } from "@/components/layout/paragraph"
 import { ProjectItem } from "@/components/project-item"
 import { RightArrowSVG } from "@/assets/icons"
-import { HoverCardItem } from "@/components/layout/hovercard"
 import { WordLink } from "@/components/layout/wordlink"
 import { Section } from "@/components/layout/section"
 import { siteConfig } from "@/config/site"
@@ -12,37 +11,10 @@ export default function Home() {
     <>
       <Section className="px-4">
         <Paragraph>
-          Hi there, most people around me call me Kev. I'm a design-focused frontend developer from
-          Barrie who enjoys dipping his toes into full stack development. Currently, I'm interested
-          in learning more about the development and uses of design systems.
+          I'm a design-focused frontend developer from Barrie who enjoys dipping his toes into full
+          stack development. My comfort zone lies where development meets design and I am always
+          refining my skillset to craft a memorable digital experience for others.
         </Paragraph>
-        {/* <Paragraph>
-          Video games have played a significant role in shaping my life, as they are able to fully
-          <HoverCardItem
-            imageUrl="/osrs.webp"
-            imageAlt="An image of Oldschool Runescape"
-            imageTitle="Oldschool Runescape"
-          >
-            immerse
-          </HoverCardItem>
-          their users in an unforgettable experience. It's my goal to recreate these
-          <HoverCardItem
-            imageUrl="/lostark.jpeg"
-            imageAlt="An image of Lost Ark"
-            imageTitle="Lost Ark"
-          >
-            captivating
-          </HoverCardItem>
-          moments and deliver an
-          <HoverCardItem
-            imageUrl="/erbs.jpg"
-            imageAlt="An image of Eternal Return"
-            imageTitle="Eternal Return"
-          >
-            exceptional
-          </HoverCardItem>
-          web experience for others.
-        </Paragraph> */}
         <Paragraph>
           I've put a strong focus on the Javascript ecosystem where I use
           <WordLink href="https://nextjs.org/" newTab={true}>
@@ -73,8 +45,8 @@ export default function Home() {
 
         <Paragraph>
           I'm actively looking for a professional home where I can contribute my dedication and
-          skills. Feel free to <WordLink href="mailto:kvo.codes@gmail.com">reach out</WordLink> and
-          say hello.
+          skills to. Feel free to <WordLink href="mailto:kvo.codes@gmail.com">reach out</WordLink>{" "}
+          and say hello.
         </Paragraph>
       </Section>
 
