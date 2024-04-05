2254f92559b0b43166de5d0e1ea742123064abe3
Kev <sulpeum@gmail.com>
2024-04-05 15:15:25 -0400

    fix: attempt to fix caching

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,24 +1,35 @@
+"use client"
+
 export const dynamic = "force-dynamic"
 
 import { Separator } from "@/components/ui/separator"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 type FooterProps = {
   className?: string
 }
 
 export const Footer = ({ className }: FooterProps) => {
-  function getCurrentTimeInToronto() {
-    const now = new Date()
-    const formatter = new Intl.DateTimeFormat("en-US", {
-      timeZone: "America/Toronto",
-      hour12: true,
-      hour: "numeric",
-      minute: "numeric",
-    })
-    return formatter.format(now)
-  }
+  const [currentTime, setCurrentTime] = useState("")
+
+  useEffect(() => {
+    function getCurrentTimeInToronto() {
+      const now = new Date()
+      const formatter = new Intl.DateTimeFormat("en-US", {
+        timeZone: "America/Toronto",
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric",
+      })
+      return formatter.format(now)
+    }
+
+    const time = getCurrentTimeInToronto()
+
+    setCurrentTime(time)
+  }, [])
 
   return (
     <>
@@ -29,7 +40,7 @@ export const Footer = ({ className }: FooterProps) => {
           className
         )}
       >
-        <span>Barrie — {getCurrentTimeInToronto()}</span>
+        <span>Barrie — {currentTime}</span>
 
         <Link
           href="/resume.pdf"
